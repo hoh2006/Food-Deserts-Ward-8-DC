96d546d70780aa448bb11a5d460a24016591bb01
hoh2006 <hoh2006@nyu.edu>
2023-12-01 14:36:34 -0500

    Updated Map and R
    Made some changes

--- FoodDesertDC.R
+++ FoodDesertDC.R
@@ -7,3 +7,5 @@ library(tidycensus)
 census_api_key("dd9da6fa26b75b06a926e9c3d8d686facefba449", install = TRUE, overwrite = TRUE)
 readRenviron("~/.Renviron")
 
+census_tract_economics <- read_csv("/Users/hhoffmann/Downloads/ACS_Economic_Characteristics_DC_Census_Tract.csv")
+
